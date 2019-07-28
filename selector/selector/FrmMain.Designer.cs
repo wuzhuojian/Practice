28dcd25e95469663738a1dcded1021dfdd83c5cb
wuzhuojian <zjwu920@hotmail.com>
2019-07-28 16:24:28 +0900

    主窗体名称修改

--- selector/selector/FrmMain.Designer.cs
+++ selector/selector/FrmMain.Designer.cs
@@ -55,9 +55,10 @@
             // 
             // btnSingleSelectSuiji
             // 
-            this.btnSingleSelectSuiji.Location = new System.Drawing.Point(564, 218);
+            this.btnSingleSelectSuiji.Location = new System.Drawing.Point(846, 349);
+            this.btnSingleSelectSuiji.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSingleSelectSuiji.Name = "btnSingleSelectSuiji";
-            this.btnSingleSelectSuiji.Size = new System.Drawing.Size(75, 35);
+            this.btnSingleSelectSuiji.Size = new System.Drawing.Size(112, 56);
             this.btnSingleSelectSuiji.TabIndex = 1;
             this.btnSingleSelectSuiji.Text = "随机单组";
             this.btnSingleSelectSuiji.UseVisualStyleBackColor = true;
@@ -65,9 +66,10 @@
             // 
             // btnSingleSelect
             // 
-            this.btnSingleSelect.Location = new System.Drawing.Point(694, 218);
+            this.btnSingleSelect.Location = new System.Drawing.Point(1041, 349);
+            this.btnSingleSelect.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSingleSelect.Name = "btnSingleSelect";
-            this.btnSingleSelect.Size = new System.Drawing.Size(75, 35);
+            this.btnSingleSelect.Size = new System.Drawing.Size(112, 56);
             this.btnSingleSelect.TabIndex = 2;
             this.btnSingleSelect.Text = "单组选号";
             this.btnSingleSelect.UseVisualStyleBackColor = true;
@@ -75,9 +77,10 @@
             // 
             // btnGroupSelect
             // 
-            this.btnGroupSelect.Location = new System.Drawing.Point(694, 301);
+            this.btnGroupSelect.Location = new System.Drawing.Point(1041, 482);
+            this.btnGroupSelect.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnGroupSelect.Name = "btnGroupSelect";
-            this.btnGroupSelect.Size = new System.Drawing.Size(75, 37);
+            this.btnGroupSelect.Size = new System.Drawing.Size(112, 59);
             this.btnGroupSelect.TabIndex = 3;
             this.btnGroupSelect.Text = "随机组选";
             this.btnGroupSelect.UseVisualStyleBackColor = true;
@@ -85,9 +88,10 @@
             // 
             // btnDel
             // 
-            this.btnDel.Location = new System.Drawing.Point(564, 376);
+            this.btnDel.Location = new System.Drawing.Point(846, 602);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnDel.Name = "btnDel";
-            this.btnDel.Size = new System.Drawing.Size(75, 34);
+            this.btnDel.Size = new System.Drawing.Size(112, 54);
             this.btnDel.TabIndex = 4;
             this.btnDel.Text = "删除";
             this.btnDel.UseVisualStyleBackColor = true;
@@ -95,9 +99,10 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(694, 376);
+            this.btnClear.Location = new System.Drawing.Point(1041, 602);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(75, 34);
+            this.btnClear.Size = new System.Drawing.Size(112, 54);
             this.btnClear.TabIndex = 5;
             this.btnClear.Text = "清除";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -105,18 +110,20 @@
             // 
             // btnShouXie
             // 
-            this.btnShouXie.Location = new System.Drawing.Point(564, 438);
+            this.btnShouXie.Location = new System.Drawing.Point(846, 701);
+            this.btnShouXie.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnShouXie.Name = "btnShouXie";
-            this.btnShouXie.Size = new System.Drawing.Size(75, 36);
+            this.btnShouXie.Size = new System.Drawing.Size(112, 58);
             this.btnShouXie.TabIndex = 6;
             this.btnShouXie.Text = "手写号码";
             this.btnShouXie.UseVisualStyleBackColor = true;
             // 
             // btnPrint
             // 
-            this.btnPrint.Location = new System.Drawing.Point(694, 438);
+            this.btnPrint.Location = new System.Drawing.Point(1041, 701);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnPrint.Name = "btnPrint";
-            this.btnPrint.Size = new System.Drawing.Size(75, 36);
+            this.btnPrint.Size = new System.Drawing.Size(112, 58);
             this.btnPrint.TabIndex = 7;
             this.btnPrint.Text = "打印";
             this.btnPrint.UseVisualStyleBackColor = true;
@@ -125,90 +132,99 @@
             // lbNumList
             // 
             this.lbNumList.FormattingEnabled = true;
-            this.lbNumList.ItemHeight = 15;
-            this.lbNumList.Location = new System.Drawing.Point(58, 218);
+            this.lbNumList.ItemHeight = 24;
+            this.lbNumList.Location = new System.Drawing.Point(87, 349);
+            this.lbNumList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.lbNumList.Name = "lbNumList";
-            this.lbNumList.Size = new System.Drawing.Size(437, 259);
+            this.lbNumList.Size = new System.Drawing.Size(654, 412);
             this.lbNumList.TabIndex = 16;
             this.lbNumList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.numListMouse_Click);
             // 
             // txtGroup
             // 
-            this.txtGroup.Location = new System.Drawing.Point(590, 309);
+            this.txtGroup.Location = new System.Drawing.Point(885, 494);
+            this.txtGroup.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtGroup.Name = "txtGroup";
-            this.txtGroup.Size = new System.Drawing.Size(49, 25);
+            this.txtGroup.Size = new System.Drawing.Size(72, 35);
             this.txtGroup.TabIndex = 18;
             this.txtGroup.Text = "5";
             this.txtGroup.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtSelector6
             // 
-            this.txtSelector6.Location = new System.Drawing.Point(577, 102);
+            this.txtSelector6.Location = new System.Drawing.Point(866, 163);
+            this.txtSelector6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSelector6.Name = "txtSelector6";
-            this.txtSelector6.Size = new System.Drawing.Size(28, 25);
+            this.txtSelector6.Size = new System.Drawing.Size(40, 35);
             this.txtSelector6.TabIndex = 19;
             this.txtSelector6.Text = "0";
             this.txtSelector6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtSelector5
             // 
-            this.txtSelector5.Location = new System.Drawing.Point(414, 102);
+            this.txtSelector5.Location = new System.Drawing.Point(621, 163);
+            this.txtSelector5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSelector5.Name = "txtSelector5";
-            this.txtSelector5.Size = new System.Drawing.Size(26, 25);
+            this.txtSelector5.Size = new System.Drawing.Size(37, 35);
             this.txtSelector5.TabIndex = 20;
             this.txtSelector5.Text = "0";
             this.txtSelector5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtSelector4
             // 
-            this.txtSelector4.Location = new System.Drawing.Point(325, 102);
+            this.txtSelector4.Location = new System.Drawing.Point(488, 163);
+            this.txtSelector4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSelector4.Name = "txtSelector4";
-            this.txtSelector4.Size = new System.Drawing.Size(27, 25);
+            this.txtSelector4.Size = new System.Drawing.Size(38, 35);
             this.txtSelector4.TabIndex = 21;
             this.txtSelector4.Text = "0";
             this.txtSelector4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtSelector3
             // 
-            this.txtSelector3.Location = new System.Drawing.Point(236, 102);
+            this.txtSelector3.Location = new System.Drawing.Point(354, 163);
+            this.txtSelector3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSelector3.Name = "txtSelector3";
-            this.txtSelector3.Size = new System.Drawing.Size(27, 25);
+            this.txtSelector3.Size = new System.Drawing.Size(38, 35);
             this.txtSelector3.TabIndex = 22;
             this.txtSelector3.Text = "0";
             this.txtSelector3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtSelector2
             // 
-            this.txtSelector2.Location = new System.Drawing.Point(149, 102);
+            this.txtSelector2.Location = new System.Drawing.Point(224, 163);
+            this.txtSelector2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSelector2.Name = "txtSelector2";
-            this.txtSelector2.Size = new System.Drawing.Size(28, 25);
+            this.txtSelector2.Size = new System.Drawing.Size(40, 35);
             this.txtSelector2.TabIndex = 23;
             this.txtSelector2.Text = "0";
             this.txtSelector2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtSelector1
             // 
-            this.txtSelector1.Location = new System.Drawing.Point(58, 102);
+            this.txtSelector1.Location = new System.Drawing.Point(87, 163);
+            this.txtSelector1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSelector1.Name = "txtSelector1";
-            this.txtSelector1.Size = new System.Drawing.Size(27, 25);
+            this.txtSelector1.Size = new System.Drawing.Size(38, 35);
             this.txtSelector1.TabIndex = 24;
             this.txtSelector1.Text = "0";
             this.txtSelector1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // txtSelector7
             // 
-            this.txtSelector7.Location = new System.Drawing.Point(680, 102);
+            this.txtSelector7.Location = new System.Drawing.Point(1020, 163);
+            this.txtSelector7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtSelector7.Name = "txtSelector7";
-            this.txtSelector7.Size = new System.Drawing.Size(29, 25);
+            this.txtSelector7.Size = new System.Drawing.Size(42, 35);
             this.txtSelector7.TabIndex = 25;
             this.txtSelector7.Text = "0";
             this.txtSelector7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 574);
+            this.ClientSize = new System.Drawing.Size(1248, 918);
             this.Controls.Add(this.txtSelector7);
             this.Controls.Add(this.txtSelector1);
             this.Controls.Add(this.txtSelector2);
@@ -225,8 +241,9 @@
             this.Controls.Add(this.btnGroupSelect);
             this.Controls.Add(this.btnSingleSelect);
             this.Controls.Add(this.btnSingleSelectSuiji);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmMain";
-            this.Text = "Form1";
+            this.Text = "彩票选择器";
             this.ResumeLayout(false);
             this.PerformLayout();
 
